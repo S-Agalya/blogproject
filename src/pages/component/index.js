be5c2d5ecacb9403b4fce29d9c865aad4c315130
S.Agalya <agalyaishere@gmail.com>
2024-01-22 00:20:52 +0530

    first commit

---
+++ src/pages/component/index.js
@@ -0,0 +1,11 @@
+import Image from 'next/image';
+function imagecomponent(){
+    return(
+        <>
+        <div>
+            <Image src="/blog/allimages/yoga.jpg width={800}
+        height={600}">
+                </Image></div></>
+    )
+}
+export default imagecomponent
